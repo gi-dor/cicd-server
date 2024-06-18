8e39c269f034d1b8b5b556d377ef9880e3cbed69
gi-dor <rltjs3563@naver.com>
2024-06-18 18:39:22 +0900

    first commit

---
+++ src/main/java/com/example/CICD_server/AppController.java
@@ -0,0 +1,15 @@
+package com.example.CICD_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/example/CICD_server/CicdServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CICD_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
